e102960ef525e4cb64089ff6ec54c4db18f234f7
theeBajber <ajrafsalim@gmail.com>
2025-11-02 16:01:26 +0300

    Added dashboard skeleton

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,26 @@
-import { SideNav } from "../components/navbar";
+import { CategoryPie, SpendingChart } from "../components/chart";
+import { DashNav } from "../components/navbar";
 
 export default function Dash() {
   return (
     <div>
-      <SideNav />
+      <DashNav />
+      <main className="relative top-16 left-50 p-4 w-[calc(100vw-200px)] flex flex-col gap-4">
+        <div className="text-left">
+          <h2 className="text-3xl font-bold">Dashboard</h2>
+          <p>Overview of your subscription spending and upcoming renewals</p>
+        </div>
+        <div className="flex justify-evenly gap-4">
+          <div className="w-1/4 h-46 border border-border hover:border-primary rounded-2xl"></div>
+          <div className="w-1/4 h-46 border border-border hover:border-primary rounded-2xl"></div>
+          <div className="w-1/4 h-46 border border-border hover:border-primary rounded-2xl"></div>
+          <div className="w-1/4 h-46 border border-border hover:border-primary rounded-2xl"></div>
+        </div>
+        <div className="flex items-center w-full gap-4 h-100">
+          <SpendingChart className="w-1/2 h-full" />
+          <CategoryPie className="w-1/2 h-full border border-border rounded-2xl" />
+        </div>
+      </main>
     </div>
   );
 }
