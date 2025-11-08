4eaa7e92cf6472354e6755630bc8cedbcb5a7a07
theeBajber <ajrafsalim@gmail.com>
2025-11-08 17:26:21 +0300

    Resolved Conflicts

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -72,7 +72,6 @@ export function NavContainer() {
   );
 }
 
-
 function SideNav({ isMobileMenuOpen, setMobileMenuOpen }) {
   return (
     <>
@@ -119,28 +118,51 @@ function SideNav({ isMobileMenuOpen, setMobileMenuOpen }) {
             <img src="/logo.png" className="h-8" />
             <h1 className="font-grotesk text-2xl">Billiance</h1>
           </a>
-          <button onClick={() => setMobileMenuOpen(false)} className="p-1 md:hidden">
+          <button
+            onClick={() => setMobileMenuOpen(false)}
+            className="p-1 md:hidden"
+          >
             <LuX className="size-6" />
           </button>
         </div>
         <nav className="w-full flex flex-col items-center gap-4 p-2">
-          <a className={`flex items-center gap-2 w-full`} href="/dashboard" onClick={() => setMobileMenuOpen(false)}>
+          <a
+            className={`flex items-center gap-2 w-full`}
+            href="/dashboard"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             <LuLayoutDashboard className="text-2xl" />
             <div className={`text-lg`}>Dashboard</div>
           </a>
-          <a className={`flex items-center gap-2 w-full`} href="/subscriptions" onClick={() => setMobileMenuOpen(false)}>
+          <a
+            className={`flex items-center gap-2 w-full`}
+            href="/subscriptions"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             <LuCreditCard className="text-2xl" />
             <div className={`text-lg`}>Subscriptions</div>
           </a>
-          <a className={`flex items-center gap-2 w-full`} href="/payments" onClick={() => setMobileMenuOpen(false)}>
+          <a
+            className={`flex items-center gap-2 w-full`}
+            href="/payments"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             <LuBadgeDollarSign className="text-2xl" />
             <div className={`text-lg`}>Payments</div>
           </a>
-          <a className={`flex items-center  gap-2 w-full`} href="/analytics" onClick={() => setMobileMenuOpen(false)}>
+          <a
+            className={`flex items-center  gap-2 w-full`}
+            href="/analytics"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             <LuChartColumn className="text-2xl" />
             <div className={`text-lg`}>Analytics</div>
           </a>
-          <a className={`flex items-center gap-2 w-full`} href="/settings" onClick={() => setMobileMenuOpen(false)}>
+          <a
+            className={`flex items-center gap-2 w-full`}
+            href="/settings"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             <LuSettings className="text-2xl" />
             <div className={`text-lg`}>Settings</div>
           </a>
@@ -162,13 +184,13 @@ function TopNav({ setMobileMenuOpen }) {
   return (
     <nav className="fixed top-0 right-0 w-full md:w-[calc(100vw-200px)] flex items-center h-16 backdrop-blur-lg bg-border/40 z-20 border-b border-border px-4 justify-between">
       {/* Mobile Menu Button */}
-      <button 
+      <button
         onClick={() => setMobileMenuOpen(true)}
         className="md:hidden p-2 !border-none"
       >
         <IoMenu className="size-6" />
       </button>
-      
+
       <div className="relative flex items-center rounded-full border border-border w-full md:w-[65%] max-w-md h-10 px-2 gap-2 focus-within:border-primary">
         <LuSearch />
         <input
@@ -177,92 +199,21 @@ function TopNav({ setMobileMenuOpen }) {
           className="w-[calc(100%-35px)] outline-none border-none bg-transparent text-sm"
         />
       </div>
-      <div className="hidden sm:flex items-center gap-4 md:gap-6">
-=======
-function SideNav() {
-  return (
-    <aside
-      className={`fixed left-0 w-50 h-screen bg-border/30 border-r border-border p-4 flex flex-col gap-6`}
-    >
-      <a className="flex items-center gap-2" href="/">
-        <img src="/logo.png" className="h-8" />
-        <h1 className="font-grotesk text-2xl">Billiance</h1>
-      </a>
-      <nav className="w-full flex flex-col items-center gap-4 p-2">
-        <a className={`flex items-center gap-2 w-full`} href="/dashboard">
-          <LuLayoutDashboard className="text-2xl" />
-          <div className={`text-lg`}>Dashboard</div>
-        </a>
-        <a className={`flex items-center gap-2 w-full`} href="/subscriptions">
-          <LuCreditCard className="text-2xl" />
-          <div className={`text-lg`}>Subscriptions</div>
-        </a>
-        <a className={`flex items-center gap-2 w-full`} href="/payments">
-          <LuBadgeDollarSign className="text-2xl" />
-          <div className={`text-lg`}>Payments</div>
-        </a>
-        <a className={`flex items-center  gap-2 w-full`} href="/analytics">
-          <LuChartColumn className="text-2xl" />
-          <div className={`text-lg`}>Analytics</div>
-        </a>
-        <a className={`flex items-center gap-2 w-full`} href="/settings">
-          <LuSettings className="text-2xl" />
-          <div className={`text-lg`}>Settings</div>
-        </a>
-      </nav>
-    </aside>
-  );
-}
-
-function TopNav() {
-  return (
-    <nav className="fixed top-0 right-0 w-[calc(100vw-200px)] flex items-center h-16 backdrop-blur-lg bg-border/40 z-20 border-b border-border px-4 justify-between">
-      <div className="relative flex items-center  rounded-full border border-border w-[65%] h-10 px-2 gap-2 focus-within:border-primary">
-        <LuSearch />
-        <input
-          type="text"
-          className=" w-[calc(100%-35px)] outline-none border-none group text-sm"
-        />
-      </div>
-      <div className="flex items-center gap-6">
-
-        <button type="button" className="!border-none">
-          <LuBell className="text-muted-foreground hover:text-foreground" />
-        </button>
-        <button type="button" className="flex items-center gap-2 !border-none">
-          <div className="rounded-full aspect-square h-10 bg-primary/50 justify-center flex items-center">
-            U
-          </div>
-
-          <div className="hidden lg:flex flex-col items-baseline ">
-
-          <div className="flex flex-col items-baseline ">
-
-            <div className="text-sm">User</div>
-            <div className="text-xs text-muted-foreground">
-              User@example.com
-            </div>
-          </div>
-        </button>
-      </div>
+      <div className="hidden sm:flex items-center gap-4 md:gap-6"></div>
     </nav>
   );
 }
 
 export function DashNav() {
-
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-  
+
   return (
     <div>
-      <SideNav isMobileMenuOpen={isMobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
+      <SideNav
+        isMobileMenuOpen={isMobileMenuOpen}
+        setMobileMenuOpen={setMobileMenuOpen}
+      />
       <TopNav setMobileMenuOpen={setMobileMenuOpen} />
-
-  return (
-    <div>
-      <SideNav />
-      <TopNav />
-
     </div>
   );
 }
